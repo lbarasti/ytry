9c4b826a95097e261e827db139b8a53c19d2c637
lorenzo.barasti <lorenzo.barasti@ocado.com>
2016-07-02 16:15:07 +0100

    Swap expectation/actual values according to asser_equal signature

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,5 +13,5 @@ def assertions; @assertions; end
 def assertions= other; @assertions = other; end
 
 def same_string(exp1)
-  -> exp2 {assert_equal(exp1.to_s, exp2); exp1}
+  -> exp2 {assert_equal(exp2, exp1.to_s); exp1}
 end
